56dca1962eddbd81edd2d533eb5208ee3ba1bab8
yts233 <62997025+yts233@users.noreply.github.com>
2022-08-28 04:37:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ async function main(){
                 startPlay();
         });
     }
+    else{
+        alert('你的浏览器不支持此页面，我也不瞒你了，这个就是一个普通的rickroll');
+        location.href='https://www.bilibili.com/video/BV1GJ411x7h7';
+    }
 }
 
 function startPlay() {
